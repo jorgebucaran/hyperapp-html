4b2cea8c501a994d3b625522adaa8977131c4fa2
Luke Jackson <lukejacksonn@gmail.com>
2017-09-07 14:15:57 +0100

    Implements optional props solution and adds test

--- src/html.js
+++ src/html.js
@@ -1,8 +1,16 @@
 import { h } from "hyperapp"
 
+const hh = function (h) {
+  return function (tag, props, children) {
+    return typeof props === 'object' && !Array.isArray(props) && props !== null
+      ? h(tag, props, children)
+      : h(tag, null, props)
+  }
+}
+
 {% for tag in htmlTags %}
 export function {{ tag }}(props, children) {
-  return h("{{ tag }}", props, children)
+  return hh(h)("{{ tag }}", props, children)
 }
 {% endfor %}
 
@@ -10,7 +18,7 @@ export default function factory(h) {
   return {
     {% for tag in htmlTags %}
     {{ tag }}: function {{ tag }}(props, children) {
-      return h("{{ tag }}", props, children)
+      return hh(h)("{{ tag }}", props, children)
     }{% if !loop.last %},{% endif %}
     {% endfor %}
   }
