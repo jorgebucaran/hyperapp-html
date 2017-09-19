ac85504f77e9163700e3f8632292aac021e853bc
Swizz <gerodel.quentin@gmail.com>
2017-09-19 10:30:37 +0200

    Update implementation without es6 and update TS declaration

--- src/html.js
+++ src/html.js
@@ -1,16 +1,18 @@
 import { h } from "hyperapp"
 
-const hh = function (h) {
-  return function (tag, props, children) {
-    return typeof props === 'object' && !Array.isArray(props)
-      ? h(tag, props, children)
-      : h(tag, null, props)
+function vnode(h) {
+  return function (tag) {
+    return function (props, children) {
+      return typeof props === 'object' && !Array.isArray(props)
+        ? h(tag, props, children)
+        : h(tag, null, props)
+    }
   }
 }
 
 {% for tag in htmlTags %}
 export function {{ tag }}(props, children) {
-  return hh(h)("{{ tag }}", props, children)
+  return vnode(h)("{{ tag }}")(props, children)
 }
 {% endfor %}
 
@@ -18,7 +20,7 @@ export default function factory(h) {
   return {
     {% for tag in htmlTags %}
     {{ tag }}: function {{ tag }}(props, children) {
-      return hh(h)("{{ tag }}", props, children)
+      return vnode(h)("{{ tag }}")(props, children)
     }{% if !loop.last %},{% endif %}
     {% endfor %}
   }
