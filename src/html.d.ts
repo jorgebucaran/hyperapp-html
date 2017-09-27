9b95c3aca26c389aa32b096c342cc4b432509ec6
Swizz <gerodel.quentin@gmail.com>
2017-09-27 13:54:49 +0200

    update jsdoc

--- src/html.d.ts
+++ src/html.d.ts
@@ -3,7 +3,7 @@ export as namespace hyperappHtml
 import { VNode } from "hyperapp"
 
 {% for tag in htmlTags %}
-/** Helper function that call the proper `h` to return a {{ tag }} virtual node
+/** Helper function that call `h` to return a {{ tag }} virtual node
  * @param props      Any valid HTML atributes, events, styles, and meta data
  * @param children  The children of the virtual node
  */
