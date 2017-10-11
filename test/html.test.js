f2f943cbd19ba4f27157140d5d1e3dfdd058d427
Jorge Bucaran <mail@jorgebucaran.com>
2017-10-12 04:07:24 +0900

    Add test for all tags.

--- test/html.test.js
+++ test/html.test.js
@@ -1,70 +1,79 @@
 import { h } from "hyperapp"
-import html, { div, h1 } from "../dist/html.js"
+import * as html from "../dist/html"
+import { htmlTags } from "../vars.json"
 
-test("hyperapp h similarity", () => {
-  expect(
-    div(null, [])
-  ).toEqual(
-    h("div", null, [])
-  )
+const { div, h1 } = html
+
+test("same as h", () => {
+  expect(div(null, [])).toEqual(h("div", null, []))
 
   expect(
-    h1({ id: "title" }, "Hi.")
+    h1(
+      {
+        id: "title"
+      },
+      "Hi."
+    )
   ).toEqual(
-    h("h1", { id: "title" }, "Hi.")
+    h(
+      "h1",
+      {
+        id: "title"
+      },
+      "Hi."
+    )
   )
 
   expect(
     div({}, [
-      h1({ id: "title" }, "Hi.")
+      h1(
+        {
+          id: "title"
+        },
+        "Hi."
+      )
     ])
   ).toEqual(
     h("div", {}, [
-      h("h1", { id: "title" }, "Hi.")
+      h(
+        "h1",
+        {
+          id: "title"
+        },
+        "Hi."
+      )
     ])
   )
 })
 
 test("optional props", () => {
+  expect(div()).toEqual(h("div", null, []))
 
-  expect(
-    div()
-  ).toEqual(
-    h("div", null, [])
-  )
+  expect(div(null, "Hi.")).toEqual(h("div", null, "Hi."))
 
-  expect(
-    div(null, "Hi.")
-  ).toEqual(
-    h("div", null, "Hi.")
-  )
+  expect(div(null, 1)).toEqual(h("div", null, 1))
 
-  expect(
-    div(null, 1)
-  ).toEqual(
-    h("div", null, 1)
-  )
+  expect(div("Hi.")).toEqual(h("div", null, ["Hi."]))
 
-  expect(
-    div("Hi.")
-  ).toEqual(
-    h("div", null, ["Hi."])
-  )
+  expect(div(1)).toEqual(h("div", null, [1]))
 
-  expect(
-    div(1)
-  ).toEqual(
-    h("div", null, [1])
-  )
-
-  expect(
-    div([
-      h1({ id: "title" }, "Hi.")
-    ])
-  ).toEqual(
+  expect(div([h1({ id: "title" }, "Hi.")])).toEqual(
     h("div", null, [
-      h("h1", { id: "title" }, "Hi.")
+      h(
+        "h1",
+        {
+          id: "title"
+        },
+        "Hi."
+      )
     ])
   )
+})
 
+test("tags", () => {
+  htmlTags.map(tag => {
+    expect(eval(`html.${tag}({ id: "foo" }, "bar")`)).toEqual(
+      h(tag, { id: "foo" }, "bar")
+    )
+  })
 })
