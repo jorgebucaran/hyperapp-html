a1331c4932d75b9a15017b3dd291888a9a0fb5e9
Jorge Bucaran <mail@jorgebucaran.com>
2018-01-14 12:22:07 +0900

    Upgrade to latest Hyperapp.

--- src/html.d.ts
+++ src/html.d.ts
@@ -2,16 +2,16 @@ export as namespace hyperappHtml
 
 import { VNode } from "hyperapp"
 
-{% for tag in htmlTags %}
-/** Utility function that calls `h` and returns a {{ tag }} VNode
+{% for name in htmlTags %}
+/** Utility function that calls `h` and returns a {{ name }} VNode
  * @param props     Any valid HTML atributes, events, styles, and meta data
  * @param children  The VNode children
  */
-export function {{ tag }}<Props = {}>(
+export function {{ name }}<Props = {}>(
   props?: Props,
   children?: string | number | Array<string | number | VNode<{}>>
 ): VNode<Props>
-export function {{ tag }}(
+export function {{ name }}(
   children?: string | number | Array<string | number | VNode<{}>>
 ): VNode<{}>
 {% endfor %}
