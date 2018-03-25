f39a91511e325dfc7d8c057d2daa921a7e522806
Jorge Bucaran <mail@jorgebucaran.com>
2018-03-25 14:19:02 +0900

    Use {} instead of null.

--- test/html.test.js
+++ test/html.test.js
@@ -5,68 +5,28 @@ import { htmlTags } from "../vars.json"
 const { div, h1 } = html
 
 test("same as h", () => {
-  expect(div(null, [])).toEqual(h("div", null, []))
+  expect(div({}, [])).toEqual(h("div", {}, []))
 
-  expect(
-    h1(
-      {
-        id: "title"
-      },
-      "Hi."
-    )
-  ).toEqual(
-    h(
-      "h1",
-      {
-        id: "title"
-      },
-      "Hi."
-    )
-  )
+  expect(h1({ id: "title" }, "Hi.")).toEqual(h("h1", { id: "title" }, "Hi."))
 
-  expect(
-    div({}, [
-      h1(
-        {
-          id: "title"
-        },
-        "Hi."
-      )
-    ])
-  ).toEqual(
-    h("div", {}, [
-      h(
-        "h1",
-        {
-          id: "title"
-        },
-        "Hi."
-      )
-    ])
+  expect(div({}, [h1({ id: "title" }, "Hi.")])).toEqual(
+    h("div", {}, [h("h1", { id: "title" }, "Hi.")])
   )
 })
 
 test("optional props", () => {
-  expect(div()).toEqual(h("div", null, []))
+  expect(div()).toEqual(h("div", {}, []))
 
-  expect(div(null, "Hi.")).toEqual(h("div", null, "Hi."))
+  expect(div({}, "Hi.")).toEqual(h("div", {}, "Hi."))
 
-  expect(div(null, 1)).toEqual(h("div", null, 1))
+  expect(div({}, 1)).toEqual(h("div", {}, 1))
 
-  expect(div("Hi.")).toEqual(h("div", null, ["Hi."]))
+  expect(div("Hi.")).toEqual(h("div", {}, ["Hi."]))
 
-  expect(div(1)).toEqual(h("div", null, [1]))
+  expect(div(1)).toEqual(h("div", {}, [1]))
 
   expect(div([h1({ id: "title" }, "Hi.")])).toEqual(
-    h("div", null, [
-      h(
-        "h1",
-        {
-          id: "title"
-        },
-        "Hi."
-      )
-    ])
+    h("div", {}, [h("h1", { id: "title" }, "Hi.")])
   )
 })
 
