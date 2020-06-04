beb8eb99b907dad2af23aaf2a421bd1da9c9b147
Maximilian Hoffmann <account@maximilianhoffmann.com>
2020-06-04 20:21:09 +0200

    update order of overloads

--- src/html.d.ts
+++ src/html.d.ts
@@ -7,14 +7,14 @@ import { VNode } from "hyperapp"
  * @param attributes Any valid HTML atributes, events, styles, and meta data
  * @param children   The VNode children
  */
-export function {{ name }}<State = {}, Actions = {}>(
-  ...params: any
-): (state: State, actions: Actions) => VNode<{}>
+export function {{ name }}(
+  children?: string | number | VNode<{}> | Array<string | number | VNode<{}>>
+): VNode<{}>
 export function {{ name }}<Attributes = {}>(
   attributes?: Attributes,
   children?: string | number | VNode<{}> | Array<string | number | VNode<{}>>
 ): VNode<Attributes>
-export function {{ name }}(
-  children?: string | number | VNode<{}> | Array<string | number | VNode<{}>>
-): VNode<{}>
+export function {{ name }}<State = {}, Actions = {}>(
+  ...params: any
+): (state: State, actions: Actions) => VNode<{}>
 {% endfor %}
