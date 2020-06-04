056865aedd2171b6e6d61768361a0542012ad569
Maximilian Hoffmann <account@maximilianhoffmann.com>
2020-06-04 19:01:18 +0200

    Add overload for lazy components
    hyperapp v1 allows creating lazy components which are curried functions receiving `state` and `actions` as arguments. this adds a third overload allowing the usage of lazy components. It also allows passing just one `VNode<{}>` as `children`, since that is possible as well.

--- src/html.d.ts
+++ src/html.d.ts
@@ -7,11 +7,14 @@ import { VNode } from "hyperapp"
  * @param attributes Any valid HTML atributes, events, styles, and meta data
  * @param children   The VNode children
  */
+export function {{ name }}<State = {}, Actions = {}>(
+  ...params: any
+): (state: State, actions: Actions) => VNode<{}>
 export function {{ name }}<Attributes = {}>(
   attributes?: Attributes,
-  children?: string | number | Array<string | number | VNode<{}>>
+  children?: string | number | VNode<{}> | Array<string | number | VNode<{}>>
 ): VNode<Attributes>
 export function {{ name }}(
-  children?: string | number | Array<string | number | VNode<{}>>
+  children?: string | number | VNode<{}> | Array<string | number | VNode<{}>>
 ): VNode<{}>
 {% endfor %}
